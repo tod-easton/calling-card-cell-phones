6fe249bd40ab7b2426f76a3fe08778153d0cb94e
tod-easton <tod.easton.fv6o@statefarm.com>
2021-07-09 09:40:32 -0700

    initial push

---
+++ main.js
@@ -0,0 +1,8 @@
+import CallingCards from './CallingCard.js';
+
+
+const CallingCard = require('./CallingCard.js');
+
+let card1 = new CallingCard(10); // card1 costs 10 cents / minute to use
+
+let card2 = new CallingCard(15); // card2 costs 15 cents / minute to use
